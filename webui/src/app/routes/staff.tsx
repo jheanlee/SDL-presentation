2080d52483b9374b507fc30b12232d165469f69e
patrick0980625 <patrick0980625@gmail.com>
2026-02-05 14:00:18 +0800

    Updated staff list

--- webui/src/app/routes/staff.tsx
+++ webui/src/app/routes/staff.tsx
@@ -1,18 +1,14 @@
-import { useTranslation } from "react-i18next";
 import { StaffItem } from "@/components/list-item/staff-item.tsx";
 
 export const Staff = () => {
-  const { t } = useTranslation();
+  const group: string[] = ['coordinator', 'music', 'performance', 'photograph', 'decoration', 'media', 'design'];
 
   return (
-    <div className="w-full h-full pl-5">
-      <div className="mt-5 mb-10 text-3xl text-stone-700 dark:text-stone-300 justify-items-left text-left break-keep">
-        <h1 className="whitespace-pre-wrap">{t("navbar.staff_list")}</h1>
-      </div>
-      <div className="flex flex-col m-8 gap-4 break-keep">
-        <StaffItem group={"coordinator_group"} />
-        <StaffItem group={"art_group"} />
-        <StaffItem group={"group"} />
+    <div className="w-full h-full p-10 break-keep">
+      <div>
+        {group.map((item, index) => (
+          <StaffItem group={item} key={index}/>
+        ))}
       </div>
     </div>
   );
